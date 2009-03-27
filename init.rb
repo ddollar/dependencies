0748f14f45dcdd39a3448d63db0b55b415df4b96
David Dollar <ddollar@gmail.com>
2009-03-27 16:03:32 -0400

    support :require_as => [ nil | false ] to stop the loading of a gem

--- init.rb
+++ init.rb
@@ -3,9 +3,13 @@ require File.join(File.dirname(__FILE__), 'lib', 'dependencies')
 deps = ::Dependencies::Reader.read_file(File.join(Rails.root, 'config', 'dependencies.rb'))
 
 deps.each do |dep|
-  require_as = dep.options[:require_as] || dep.name
+  dep.options[:require_as] = dep.name unless dep.options.has_key?(:require_as)
+
   begin
-    require require_as
+    case dep.options[:require_as]
+      when NilClass, FalseClass then next
+      else require dep.options[:require_as]
+    end
   rescue LoadError
     puts "was unable to require #{dep.name} as '#{require_as}'"
   end
