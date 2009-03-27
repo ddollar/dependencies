b3299c403ffff2e0411cc8d7ba6b0584713eeac5
David Dollar <ddollar@gmail.com>
2009-03-27 16:27:52 -0400

    support :env option to dependencies

--- init.rb
+++ init.rb
@@ -3,13 +3,24 @@ require File.join(File.dirname(__FILE__), 'lib', 'dependencies')
 deps = ::Dependencies::Reader.read_file(File.join(Rails.root, 'config', 'dependencies.rb'))
 
 deps.each do |dep|
-  dep.options[:require_as] = dep.name unless dep.options.has_key?(:require_as)
+  current_environment = ENV['RAILS_ENV'] || 'development'
+
+  options = {
+    :env        => [current_environment],
+    :require_as => dep.name
+  }.merge(dep.options)
+
+  # swap their :env to an array if they used a string
+  options[:env] = [options[:env]] unless options[:env].is_a?(Array)
+
+  # don't run if require_as is nil or false
+  next if [nil, false].include?(options[:require_as])
+
+  # don't run if the gem wants an env that is not the current one
+  next unless options[:env].include?(current_environment)
 
   begin
-    case dep.options[:require_as]
-      when NilClass, FalseClass then next
-      else require dep.options[:require_as]
-    end
+    require options[:require_as]
   rescue LoadError
     puts "was unable to require #{dep.name} as '#{require_as}'"
   end
