223b59fe8151381a5fc405746e651512da8328fc
David Dollar <ddollar@gmail.com>
2009-04-15 16:45:00 -0400

    support with_options

--- lib/dependencies/reader.rb
+++ lib/dependencies/reader.rb
@@ -14,10 +14,22 @@ class Dependencies::Reader
 
   def initialize
     @dependencies = []
+    @with_options = []
   end
 
   def dependency(name, *options)
+    if @with_options.last
+      opts = options.last.is_a?(Hash) ? options.pop : {}
+      @with_options.reverse.each { |wo| opts = wo.merge(opts) }
+      options.push(opts)
+    end
     @dependencies << ::Dependencies::Dependency.new(name, *options)
+  end
+
+  def with_options(options={}, &block)
+    @with_options.push(options)
+    yield
+    @with_options.pop
   end
 
 end
