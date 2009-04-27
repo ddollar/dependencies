a959aeed74718d6929aeafc97c1900c2bc2525b0
David Dollar <ddollar@gmail.com>
2009-04-27 11:31:20 -0400

    consolidate ext files

--- lib/ext/rubygems.rb
+++ lib/ext/rubygems.rb
@@ -1,6 +1,7 @@
 require 'erb'
 require 'rubygems'
 require 'rubygems/dependency_installer'
+require 'rubygems/source_index'
 
 Gem.pre_install_hooks.push(proc do |installer|
   unless File.file?(File.join(installer.bin_dir, "common.rb"))
@@ -121,5 +122,69 @@ class ::Gem::Specification
       spec
     end
     specs + specs.map {|s| s.recursive_dependencies(self, index)}.flatten.uniq
+  end
+end
+
+module Gem
+  BUNDLED_SPECS = File.join(Dir.pwd, "gems", "specifications")
+  MAIN_INDEX = Gem::SourceIndex.from_gems_in(BUNDLED_SPECS)
+  FALLBACK_INDEX = Gem::SourceIndex.from_installed_gems
+
+  def self.source_index
+    MultiSourceIndex.new
+  end
+
+  def self.searcher
+    MultiPathSearcher.new
+  end
+
+  class ArbitrarySearcher < GemPathSearcher
+    def initialize(source_index)
+      @source_index = source_index
+      super()
+    end
+
+    def init_gemspecs
+      @source_index.map { |_, spec| spec }.sort { |a,b|
+        (a.name <=> b.name).nonzero? || (b.version <=> a.version)
+      }
+    end
+  end
+
+  class MultiPathSearcher
+    def initialize
+      @main_searcher = ArbitrarySearcher.new(MAIN_INDEX)
+      @fallback_searcher = ArbitrarySearcher.new(FALLBACK_INDEX)
+    end
+
+    def find(path)
+      try = @main_searcher.find(path)
+      return try if try
+      @fallback_searcher.find(path)
+    end
+
+    def find_all(path)
+      try = @main_searcher.find_all(path)
+      return try unless try.empty?
+      @fallback_searcher.find_all(path)
+    end
+  end
+
+  class MultiSourceIndex
+    # Used by merb.thor to confirm; not needed when MSI is in use
+    def load_gems_in(*args)
+    end
+
+    def search(*args)
+      try = MAIN_INDEX.search(*args)
+      return try unless try.empty?
+      FALLBACK_INDEX.search(*args)
+    end
+
+    def find_name(*args)
+      try = MAIN_INDEX.find_name(*args)
+      return try unless try.empty?
+      FALLBACK_INDEX.find_name(*args)
+    end
   end
 end
