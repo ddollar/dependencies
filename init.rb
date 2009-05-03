1bdf333ade1bcbc3d74feb7762398b9751d1cf84
David Dollar <ddollar@gmail.com>
2009-05-03 12:54:00 -0400

    allow dependencies init to be sidestepped through a global (gross)

--- init.rb
+++ init.rb
@@ -1,37 +1,41 @@
 require File.join(File.dirname(__FILE__), 'lib', 'dependencies')
 
-Gem.clear_paths
-Gem.path.unshift(File.join(Rails.root, 'gems'))
+unless $block_dependencies_plugin_init
 
-deps = ::Dependencies::Reader.read_file(File.join(Rails.root, 'config', 'dependencies.rb'))
+  Gem.clear_paths
+  Gem.path.unshift(File.join(Rails.root, 'gems'))
 
-deps.each do |dep|
-  current_environment = ENV['RAILS_ENV'] || 'development'
+  deps = ::Dependencies::Reader.read_file(File.join(Rails.root, 'config', 'dependencies.rb'))
 
-  options = {
-    :only       => [current_environment],
-    :except     => [],
-    :require_as => dep.name
-  }.merge(dep.options)
+  deps.each do |dep|
+    current_environment = ENV['RAILS_ENV'] || 'development'
 
-  # swap their :only and :except to an array if they used a not-Array
-  [ :only, :except ].each do |option|
-    options[option] = [options[option].to_s] unless options[option].is_a?(Array)
+    options = {
+      :only       => [current_environment],
+      :except     => [],
+      :require_as => dep.name
+    }.merge(dep.options)
+
+    # swap their :only and :except to an array if they used a not-Array
+    [ :only, :except ].each do |option|
+      options[option] = [options[option].to_s] unless options[option].is_a?(Array)
+    end
+
+    # don't run if require_as is nil or false
+    next if [nil, false].include?(options[:require_as])
+
+    # don't run if the dependency wants an env that is not the current one
+    next unless options[:only].include?(current_environment)
+
+    # don't run if the dependency does not want to load in the current env
+    next if options[:except].include?(current_environment)
+
+    begin
+      require options[:require_as]
+    rescue LoadError => e
+      puts "was unable to require #{dep.name} as '#{options[:require_as]}'
+      Reason: #{e.class.name} error raised with message: #{e.message}"
+    end
   end
 
-  # don't run if require_as is nil or false
-  next if [nil, false].include?(options[:require_as])
-
-  # don't run if the dependency wants an env that is not the current one
-  next unless options[:only].include?(current_environment)
-
-  # don't run if the dependency does not want to load in the current env
-  next if options[:except].include?(current_environment)
-
-  begin
-    require options[:require_as]
-  rescue LoadError => e
-    puts "was unable to require #{dep.name} as '#{options[:require_as]}'
-    Reason: #{e.class.name} error raised with message: #{e.message}"
-  end
 end
